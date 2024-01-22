44ab4b5950577c3f06d0cdf252d0bd281386db7f
Maicol Kutchoukov <147729218+maicolkutchoukov@users.noreply.github.com>
2024-01-22 13:06:11 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+import axios from 'axios';
+export default {
+    data() {
+        return {
+
+        };
+    },
+    components: {
+        AppHeader,
+        AppMain,
+        AppFooter
+    },  
+    methods: {
+
+    },
+    mounted(){
+        axios.get('https://rickandmortyapi.com/api/character') // URL DELL'API
+            .then((response) => {
+                console.log(response);                         // Stampo il risultato della chiamata dell'api
+                console.log(response.data.results);            // STUDIANDO il risultato trovo il percorso da seguire
+
+            })
+    }
+}
+</script>
+
+<template>
+    <h1 class="">
+        Mio Template
+    </h1>
+
+    <AppHeader />
+
+    <AppMain />
+
+    <AppFooter />
+</template>
+
+<style lang="scss">
+@use "assets/scss/main" as *;
+</style>
